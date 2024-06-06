7ea2fe35a2abed437931c9fc3824d9a6a575a88e
Asep Trisna Setiawan <aseptrisna1995@gmail.com>
2024-06-06 09:59:56 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const fs = require(`fs`);
+const http = require("https");
+
+
+const app = express();
+const botToken = process.env.TOKEN;
+const apiUrl = `${process.env.API_URL}${botToken}`;
+
+const privateKey = fs.readFileSync(process.env.KEY_PEM, "utf8");
+const certificate = fs.readFileSync(process.env.CERT_PEM, "utf8");
+const ca = fs.readFileSync(process.env.CERT_PEM, "utf8");
+
+const credentials = {
+    rejectUnauthorized: true,
+    key: privateKey,
+    cert: certificate,
+    passphrase: process.env.PFX_PASSPHRASE,
+    ca: ca,
+};
+
+app.use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {
+    const chatId = req.body.message.chat.id;
+    const message = req.body.message.text;
+
+    sendMessage(chatId, `ID chat Anda adalah ${chatId}`);
+
+    res.send('ok');
+});
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello, this is your JSON response!',
+        status: 'success',
+        data: {
+            info: 'You can add more detailed information here.'
+        }
+    });
+});
+
+function sendMessage(chatId, text) {
+    axios.post(`${apiUrl}/sendMessage`, {
+        chat_id: chatId,
+        text: text
+    })
+        .then(response => {
+            if (response.data.ok) {
+                console.log('Pesan berhasil dikirim');
+            } else {
+                console.log('Gagal mengirim pesan:', response.data);
+            }
+        })
+        .catch(error => {
+            console.error('Kesalahan saat mengirim pesan:', error);
+        });
+}
+
+const PORT = process.env.PORT || 5000;
+const server = http.createServer(credentials, app);
+
+server.listen(PORT, () => {
+    console.log(`Server berjalan di port ${PORT}`);
+});
